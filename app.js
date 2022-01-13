9382a120bb7cadf84d028a6fd3c1a68c0fad4dcb
Hailay <hailaygidey5@gmail.com>
2022-01-13 09:12:45 -0600

    clean v.0.0

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 // 3- Write an express application that returns the main page for the MEAN Games application.
 const express = require('express');
 const path = require('path');
-const router = require('./api/routes/games-route');
 
 require('dotenv').config();
 
 const app = express();
 
-//serves index.html
-app.get('/', express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
 
-// app.use((req, res, next) => {
-//     console.log(req.method, req.url);
-//     next();
-// });
 //serve static pages: index.html, page1.html, page2.html
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api', router);
 
 const server = app.listen(process.env.PORT, ()=>{
     const port = server.address().port;
